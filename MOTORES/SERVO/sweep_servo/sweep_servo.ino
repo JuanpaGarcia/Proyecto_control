34005db4e563b01aba3cb6339c44f64da8363a4b
Juanpa <garciaperezjuanpablo7@gmail.com>
2023-05-24 21:57:57 -0600

    Last aprt

--- MOTORES/SERVO/sweep_servo/sweep_servo.ino
+++ MOTORES/SERVO/sweep_servo/sweep_servo.ino
@@ -3,7 +3,7 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
+int pos = 90;    // variable to store the servo position
 
 void setup() {
   myservo.attach(25);  // attaches the servo on pin 13 to the servo object
